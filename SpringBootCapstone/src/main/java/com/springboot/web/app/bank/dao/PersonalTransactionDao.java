9efe92187bbcf3d73f8b6c35ab09d5dd953977c1
Harshad Kalantri <34030078+harshadkalantri97@users.noreply.github.com>
2021-06-02 14:24:41 +0530

    Delete SpringBootCapstone directory

--- SpringBootCapstone/src/main/java/com/springboot/web/app/bank/dao/PersonalTransactionDao.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.web.app.bank.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.web.app.bank.model.PersonalTransaction;
-
-public interface PersonalTransactionDao extends JpaRepository<PersonalTransaction, Integer>{
-
-}
